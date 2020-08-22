fb9753ed980959eab7376334eee1c816285409dc
Green-Avocado <greenavocado@protonmail.com>
2020-08-22 14:03:43 -0600

    feat: set persistence on login/register

--- front-end/login.js
+++ front-end/login.js
@@ -1,10 +1,23 @@
+function initPage() {
+}
+
 function login() {
     var email = document.getElementById("authEmail").value;
     var pass = document.getElementById("authPass").value;
 
-    firebase.auth().signInWithEmailAndPassword(email, pass).then(() => {
-        alert('success');
-    })
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistance.SESSION)
+        .then(() => {
+            firebase.auth().signInWithEmailAndPassword(email, pass)
+                .then(() => {
+                    alert('success');
+                })
+                .catch((error) => {
+                    let errorCode = error.code;
+                    let errorMessage = error.message;
+                    console.log(errorCode, errorMEssage);
+                    alert(errorMessage);
+                });
+        })
         .catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
--- front-end/register.js
+++ front-end/register.js
@@ -2,9 +2,19 @@ function register() {
     var email = document.getElementById("authEmail").value;
     var pass = document.getElementById("authPass").value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, pass).then(() => {
-        alert('success');
-    })
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistance.SESSION)
+        .then(() = {
+            firebase.auth().createUserWithEmailAndPassword(email, pass)
+                .then(() => {
+                alert('success');
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                console.log(errorCode, errorMEssage);
+                alert(errorMessage);
+            });
+        })
         .catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
