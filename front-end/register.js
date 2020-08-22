c4064a570f59d6e1f3de0414d48470d988a0b404
Green-Avocado <greenavocado@protonmail.com>
2020-08-22 14:18:07 -0600

    feat: added dummy validation

--- front-end/register.js
+++ front-end/register.js
@@ -2,24 +2,42 @@ function register() {
     var email = document.getElementById("authEmail").value;
     var pass = document.getElementById("authPass").value;
 
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistance.SESSION)
-        .then(() = {
-            firebase.auth().createUserWithEmailAndPassword(email, pass)
-                .then(() => {
-                    alert('success');
-                })
-                .catch((error) => {
-                    let errorCode = error.code;
-                    let errorMessage = error.message;
-                    console.log(errorCode, errorMEssage);
-                    alert(errorMessage);
-                });
-        })
-        .catch((error) => {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            console.log(errorCode, errorMEssage);
-            alert(errorMessage);
-        });
+    if(fieldsValid()) {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistance.SESSION)
+            .then(() = {
+                firebase.auth().createUserWithEmailAndPassword(email, pass)
+                    .then(() => {
+                        populateUserData();
+                        alert('success');
+                    })
+                    .catch((error) => {
+                        let errorCode = error.code;
+                        let errorMessage = error.message;
+                        console.log(errorCode, errorMEssage);
+                        alert(errorMessage);
+                    });
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                console.log(errorCode, errorMEssage);
+                alert(errorMessage);
+            });
+    }
+    else {
+        alert("Incomplete form");
+    }
+}
+
+function fieldsValid() {
+    var valid = true;
+
+    //TODO
+
+    return valid;
+}
+
+function populateUserData() {
+    //TODO
 }
 
