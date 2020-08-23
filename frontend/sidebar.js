5ba542f607d63343659c8166b4d9d844e5d9a07d
churrowo <li.cheryl.2706@gmail.com>
2020-08-22 20:29:46 -0600

    Profile Page
    took out home and events options

--- frontend/sidebar.js
+++ frontend/sidebar.js
@@ -3,7 +3,36 @@ function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
   }
   
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
